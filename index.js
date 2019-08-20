4b320cc0f5c859e11901b59c29ca1ab94d8a4dc9
Vladislav Baranovskiy <vladyn4uk@gmail.com>
2019-08-21 02:58:24 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+
+var TelegramBot = require('node-telegram-bot-api');
+var request = require('request');
+var token = '911070260:AAEtPZZc97XRuGcc8ORA2RigooiHl7FM90A';
+var botOptions = {   polling: true };
+var bot = new TelegramBot(token, botOptions);
+
+bot.on('text', function(msg) {
+    var messageChatId = msg.chat.id;
+    var messageText = msg.text;
+
+
+    request({url: 'https://api.nanopool.org/v1/eth/reportedhashrate/0x30ff4880c9444af729d58a463728214411f11700/rx570', json: true},
+        function(err, res, json) {
+        if (err) {
+            throw err;
+        }
+            if (messageText === '/test') {
+                bot.sendMessage(messageChatId, 'rx570' + 'Hashrate:' + json.data + '     ' + 'Статус:'+ json.status);
+            }
+    });
+
+    request({url: 'https://api.nanopool.org/v1/eth/reportedhashrate/0x30ff4880c9444af729d58a463728214411f11700/mama', json: true},
+        function(err, res, json) {
+            if (err) {
+                throw err;
+            }
+
+            if (messageText === '/test') {
+                bot.sendMessage(messageChatId, 'mama' + 'Hashrate:' + json.data + '     ' + 'Статус:'+ json.status);
+            } });
+
+    request({url: 'https://api.nanopool.org/v1/eth/reportedhashrate/0x30ff4880c9444af729d58a463728214411f11700/mamalena', json: true},
+        function(err, res, json) {
+            if (err) {
+                throw err;
+            }
+
+            if (messageText === '/test') {
+                bot.sendMessage(messageChatId, 'mamalena' + 'Hashrate:' + json.data + '     ' + 'Статус:'+ json.status);
+            } });
+
+    request({url: 'https://api.nanopool.org/v1/eth/reportedhashrate/0x30ff4880c9444af729d58a463728214411f11700/larysa', json: true},
+        function(err, res, json) {
+            if (err) {
+                throw err;
+            }
+
+            if (messageText === '/test') {
+                bot.sendMessage(messageChatId, 'larysa' + 'Hashrate:' + json.data + '     ' + 'Статус:'+ json.status);
+            } });
+
+
+});
